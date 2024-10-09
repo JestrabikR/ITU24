e0ee99372861320ea61b889e51fec8a695e74b49
Ondřej Hruboš <hruboson@gmail.com>
2024-10-09 12:42:19 +0200

    feat(get single trip): added uuid to Trip, search by id

--- api/main.py
+++ api/main.py
@@ -14,24 +14,41 @@ from country import Country
 
 app = Flask(__name__)
 
+def json_from_file(file):
+    ret = None 
+    with open(file, "r") as f:
+        data = f.read()    
+        ret = json.loads(data)
+
+    return ret
+
 @app.route("/")
 def home():
     return "<style>body{background: black; color: white}</style><a href='/trips'>Trips endpoint</a>"
 
 @app.route("/trips")
-def trip():
+def trips():
     t = Trip("test", date(2022, 3, 3), date(2022, 4, 4), 9999, "Ahoj")
     r = Response(response=t.to_json(), status=200, mimetype="application/json")
     return r
 
+@app.route("/trip/<id>")
+def trip(id):
+    trips = json_from_file("./trips.json")
+    
+    for trip in trips:
+        if(trip["id"] == id):
+            return make_response(jsonify(trip), 200)
+    
+    return make_response("Trip not found", 204)
+    
+
+
 @app.route("/trip/add", methods=["POST"])
 def add_trip():
-    trips = []
-    with open("./trips.json", "r") as trips_file:
-        data = trips_file.read()    
-        trips = json.loads(data)
-
     post_data = json.loads(request.data.decode('utf8').replace("'", '"'))
+
+    trips = json_from_file("./trips.json")
 
     trip = None
     try:
--- api/trip.py
+++ api/trip.py
@@ -1,4 +1,5 @@
 import json
+import uuid
 
 class Trip:
     """
@@ -6,6 +7,7 @@ class Trip:
     """
 
     def __init__(self, name, from_date, until_date, budget, description, subtrips=[], photos=[], advantages=[], disadvantages=[]):
+        self.id = uuid.uuid4()
         self.name = name
         self.from_date = from_date
         self.until_date = until_date
@@ -18,9 +20,12 @@ class Trip:
 
     def to_json(self):
         return {
+            "id": self.id,
             "name": self.name,
             "from_date": self.from_date,
-            "until_date": self.until_date
+            "until_date": self.until_date,
+            "budget": self.budget,
+            "description": self.description
         }
 
 class Subtrip:
