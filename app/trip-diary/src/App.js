b1baadecad017d02b4672bc0c6a9c0daa1d19883
Dominik <dominikborek@seznam.cz>
2024-10-10 14:49:43 +0200

    edited main page view

--- app/trip-diary/src/App.js
+++ app/trip-diary/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Header from './Header';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import dayjs from "dayjs";
 import './App.css';
 
 function App() {
@@ -18,13 +19,18 @@ function App() {
       .catch((error) => console.error('Error fetching trips:', error));
   }, []);
 
+  const today = dayjs();
+  const pastTrips = trips.filter(trip => dayjs(trip.until_date).isBefore(today)).slice(-3); // Minulé cesty, max 3
+  const futureTrips = trips.filter(trip => dayjs(trip.until_date).isAfter(today)); // Plánované cesty
+
   return (
-    <div className="App" style={{ minHeight: '100vh' }}>
+    <div className="App" style={{ minHeight: '100vh' }}> 
       <Header />
       <div>
-        <h1>Trips</h1>
+        {/* Minulé cesty */}
+        <h2>Minulé cesty</h2>
         <div className="trips-grid">
-          {trips.map((trip, index) => (
+          {pastTrips.map((trip, index) => (
             <Card key={index} className="trip-card" sx={{ borderRadius: '16px' }}>
               <div
                 className="trip-image"
@@ -35,7 +41,28 @@ function App() {
                 <CardContent className="trip-content">
                   <h3>{trip.name}</h3>
                   <p>{trip.country || 'Unknown country'}</p>
-                  <p>{trip.from_date} - {trip.until_date}</p>
+                  <p>{dayjs(trip.from_date).format("YYYY.MM.DD")} - {dayjs(trip.until_date).format("YYYY.MM.DD")}</p>
+                </CardContent>
+              </div>
+            </Card>
+          ))}
+        </div>
+
+        {/* Plánované cesty */}
+        <h2>Plánované cesty</h2>
+        <div className="trips-grid">
+          {futureTrips.map((trip, index) => (
+            <Card key={index} className="trip-card" sx={{ borderRadius: '16px' }}>
+              <div
+                className="trip-image"
+                style={{
+                  backgroundImage: `url(${trip.photos.length ? `data:image/jpeg;base64,${trip.photos[0]}` : 'default-image.jpg'})`,
+                }}
+              >
+                <CardContent className="trip-content">
+                  <h3>{trip.name}</h3>
+                  <p>{trip.country || 'Unknown country'}</p>
+                  <p>{dayjs(trip.from_date).format("YYYY.MM.DD")} - {dayjs(trip.until_date).format("YYYY.MM.DD")}</p>
                 </CardContent>
               </div>
             </Card>
