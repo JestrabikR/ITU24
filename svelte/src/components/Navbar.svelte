6974a2e2fa14f95a20f5f87f159c25a6a0a52d31
Ondřej Hruboš <hruboson@gmail.com>
2024-10-15 16:48:35 +0200

    feat(plus button)

--- svelte/src/components/Navbar.svelte
+++ svelte/src/components/Navbar.svelte
@@ -2,9 +2,14 @@
 	import Logo from "$lib/assets/logo.svg";
 </script>
 
+<div class="space"></div>
 <nav>
 	<img class="circle" src={Logo} alt="Logo">
 	<a href="/"><h3><b><span class="underline">Trips</span></b></h3></a>
 	<a href="/"><h3><b>Visited</b></h3></a>
+    <div class="max"></div>
+    <button class="square round extra">
+      <i>add</i>
+    </button>
 </nav>
 
