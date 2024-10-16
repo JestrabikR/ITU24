bfb4bd9e89a925e684c401956924962613116f5e
Ondřej Hruboš <hruboson@gmail.com>
2024-10-16 11:29:19 +0200

    feat(view transition API): basic, should work on Edge, Chrome (not Firefox)

---
+++ svelte/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+import { onNavigate } from '$app/navigation';
+
+// does not seem to work on Firefox (view transition API)
+onNavigate((navigation) => {
+	if (!document.startViewTransition) return;
+
+	return new Promise((resolve) => {
+		document.startViewTransition(async () => {
+			resolve();
+			await navigation.complete;
+		});
+	});
+});
+</script>
+
+<slot />
+
