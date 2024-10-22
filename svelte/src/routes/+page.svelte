5c6ae368354adfc021f4b144551d5daa35d4aa1a
Ondřej Hruboš <hruboson@gmail.com>
2024-10-22 14:46:28 +0200

    feat(trip grids categories): past/future/ongoing trip categories on main page

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -3,17 +3,66 @@
 	import Card from "@components/Card.svelte";
 
 	/** @type {import('./$types').PageData} */
-	export let data;
+	export let data = [];
+
+	const today = new Date();
+	var pastTrips = data.trips.filter(trip => new Date(trip.until_date) < today);
+	var ongoingTrips = data.trips.filter(trip => new Date(trip.from_date) <= today && new Date(trip.until_date) >= today);
+	var futureTrips = data.trips.filter(trip => new Date(trip.from_date) > today);
 </script>
 
 <main class="responsive">
 	<Navbar/>
 
-	<div class="grid medium-space">
-		{#each data.trips as card}
-			<div class="s12 m6 l4">
-				<Card title={card.name} description={card.description} imageUrl={card.photos[0]} id={card.id} />
-			</div>
-		{/each}
+	<div class="row">
+		<h3><b>Ongoing trips</b></h3>
+	</div>
+	<div class="row">
+		{#if ongoingTrips.length < 1}
+			<p class="italic">You have no ongoing trips</p>
+		{:else}
+		<div class="grid medium-space">
+			{#each ongoingTrips as card}
+				<div class="s12 m6 l4">
+					<Card title={card.name} description={card.description} imageUrl={card.photos[0]} id={card.id} />
+				</div>
+			{/each}
+		</div>
+		{/if}
+	</div>
+
+	<div class="row">
+		<h3><b>Future trips</b></h3>
+	</div>
+	<div class="row">
+		{#if futureTrips.length < 1}
+			<p class="italic">You have no ongoing trips</p>
+		{:else}
+		<div class="grid medium-space">
+			{#each futureTrips as card}
+				<div class="s12 m6 l4">
+					<Card title={card.name} description={card.description} imageUrl={card.photos[0]} id={card.id} />
+				</div>
+			{/each}
+		</div>
+		{/if}
+	</div>
+
+
+	<div class="row">
+		<h3><b>Past trips</b></h3>
+	</div>
+	<div class="row">
+		{#if pastTrips.length < 1}
+			<p class="italic">You have no ongoing trips</p>
+		{:else}
+		<div class="grid medium-space">
+			{#each pastTrips as card}
+				<div class="s12 m6 l4">
+					<Card title={card.name} description={card.description} imageUrl={card.photos[0]} id={card.id} />
+				</div>
+			{/each}
+		</div>
+		{/if}
 	</div>
 </main>
