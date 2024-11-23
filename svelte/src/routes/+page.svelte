bbf2d333370724160c933b68597f6b3a4b79d411
Ondřej Hruboš <hruboson@gmail.com>
2024-11-23 10:51:49 +0100

    fix(map view homepage): starting coordinates and zoom

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -18,7 +18,7 @@
 		if (!map) {
 			map = L.map("map", {
 				worldCopyJump: true,
-			});	
+			}).setView([26.40, -30.67], 2.5);	
 			L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 				maxZoom: 19,
 				attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
